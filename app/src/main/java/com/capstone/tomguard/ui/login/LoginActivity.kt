e0e5ed69c4c821b3f7ed291f703d66f3b763eb6a
Imam Munif <imammunif17@gmail.com>
2024-06-10 16:40:46 +0700

    implement the login logic (still can't be used)

--- app/src/main/java/com/capstone/tomguard/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/tomguard/ui/login/LoginActivity.kt
@@ -1,21 +1,115 @@
 package com.capstone.tomguard.ui.login
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.tomguard.R
+import com.capstone.tomguard.ui.ViewModelFactory
+import com.capstone.tomguard.data.Result
+import com.capstone.tomguard.data.pref.UserModel
+import com.capstone.tomguard.databinding.ActivityLoginBinding
+import com.capstone.tomguard.ui.main.MainActivity
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private val viewModel by viewModels<LoginViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
         }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            viewModel.login(email, password).observe(this) { result ->
+                if (result != null) {
+                    when (result) {
+                        is Result.Loading -> {
+                            showLoading(true)
+                            Log.d("Debug: login", "login: logging in...")
+                        }
+
+                        is Result.Success -> {
+                            showLoading(false)
+                            val loginResponse = result.data
+                            // refers to LoginResponse object returned from API when it successful
+                            showToast(loginResponse.message)
+
+                            val userModel = UserModel(
+                                email = email,
+                                token = loginResponse.loginResult.token,
+                                isLogin = true
+                            )
+                            viewModel.saveSession(userModel)
+
+                            Log.d("Debug: login", "login: login success...")
+                            Log.d("Debug: login", "current loginResponse: {$loginResponse}")
+                            Log.d("Debug: login", "current userModel: {$userModel}")
+
+                            AlertDialog.Builder(this).apply {
+                                setTitle("Success!")
+                                setMessage("Login success")
+                                setPositiveButton("Next") { _, _ ->
+                                    val intent = Intent(context, MainActivity::class.java)
+                                    intent.flags =
+                                        Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                                    startActivity(intent)
+                                    finish()
+                                }
+                                create()
+                                show()
+                            }
+                        }
+
+                        is Result.Error -> {
+                            showLoading(false)
+                            showToast(result.error)
+                            Log.d("Debug: login", "login: login failed...!!")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
