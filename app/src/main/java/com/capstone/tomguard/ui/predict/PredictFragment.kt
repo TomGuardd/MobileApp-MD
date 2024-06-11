00933e40a5c58c36b670c3d8cb1490051bffc4ec
Imam Munif <imammunif17@gmail.com>
2024-06-11 15:37:05 +0700

    create predict fragment ui

--- app/src/main/java/com/capstone/tomguard/ui/predict/PredictFragment.kt
+++ app/src/main/java/com/capstone/tomguard/ui/predict/PredictFragment.kt
@@ -35,7 +35,7 @@ class PredictFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        return inflater.inflate(R.layout.fragment_predict, container, false)
     }
 
     companion object {
